409ed3c7c33aac7585158101a708db2e87f82e3a
AndyShih <andyclock90108@gmail.com>
2025-01-22 11:47:09 +0800

    feat:建立Response Model

---
+++ DataAccess/Repository/ResponseModel/ApiResponse.cs
@@ -0,0 +1,56 @@
+﻿using Common.Enums;
+
+namespace DataAccess.Repository.ResponseModel
+{
+    /// <summary>
+    /// 無回傳DTO
+    /// </summary>
+    public class ApiResponse
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; } = new List<string>();
+        public ApiStatusCode StatusCode { get; set; }
+
+        public static ApiResponse Create(ApiStatusCode statusCode, string message = null, List<string> errors = null)
+        {
+            return new ApiResponse
+            {
+                IsSuccess = statusCode < ApiStatusCode.BadRequest,
+                StatusCode = statusCode,
+                Message = message,
+                Errors = errors
+            };
+        }
+    }
+
+    /// <summary>
+    /// 有回傳DTO
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ApiResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public List<string> Errors { get; set; } = new List<string>();
+        public ApiStatusCode StatusCode { get; set; }
+
+        public ApiResponse()
+        {
+
+        }
+
+        public static ApiResponse<T> Create(ApiStatusCode statusCode, T data = default, string message = null, List<string> errors = null)
+        {
+            return new ApiResponse<T>
+            {
+                IsSuccess = statusCode < ApiStatusCode.BadRequest,
+                StatusCode = statusCode,
+                Message = message,
+                Data = data,
+                Errors = errors
+            };
+        }
+    }
+}
---
+++ DataAccess/Repository/ResponseModel/ApiResponseFactory.cs
@@ -0,0 +1,50 @@
+﻿using Common.Enums;
+using Common.Extensions;
+
+namespace DataAccess.Repository.ResponseModel
+{
+    public class ApiResponseFactory
+    {
+        public static ApiResponse CreateErrorResult(ErrorCode errorCode)
+        {
+            return new ApiResponse
+            {
+                IsSuccess = false,
+                Message = EnumExtensions.GetEnumDescription(errorCode),
+                StatusCode = ApiStatusCode.BadRequest
+            };
+        }
+
+        public static ApiResponse CreateSuccessResult()
+        {
+            return new ApiResponse
+            {
+                IsSuccess = true,
+                Message = "Success",
+                StatusCode = ApiStatusCode.OK
+            };
+        }
+
+        public static ApiResponse<T> CreateErrorResult<T>(ErrorCode errorCode, T data = default)
+        {
+            return new ApiResponse<T>
+            {
+                IsSuccess = false,
+                Message = EnumExtensions.GetEnumDescription(errorCode),
+                Data = data,
+                StatusCode = ApiStatusCode.BadRequest
+            };
+        }
+
+        public static ApiResponse<T> CreateSuccessResult<T>(T data)
+        {
+            return new ApiResponse<T>
+            {
+                IsSuccess = true,
+                Message = "Success",
+                Data = data,
+                StatusCode = ApiStatusCode.OK
+            };
+        }
+    }
+}
